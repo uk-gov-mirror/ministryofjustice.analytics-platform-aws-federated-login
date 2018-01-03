8809fa5b0a0d3fc48de0dde4e931e75f52af0fe5
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2018-01-03 16:14:57 +0000

    Hardcoded default callback URL

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ const strategy = new Auth0Strategy(
     domain: process.env.AUTH0_DOMAIN,
     clientID: process.env.AUTH0_CLIENT_ID,
     clientSecret: process.env.AUTH0_CLIENT_SECRET,
-    callbackURL: process.env.AUTH0_CALLBACK_URL ||
-                  `http://localhost:${process.env.PORT}/callback`,
+    callbackURL:  process.env.AUTH0_CALLBACK_URL ||
+                  `http://localhost:3000/callback`,
     passReqToCallback: true,
   },
   ((
